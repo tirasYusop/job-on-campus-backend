6ce63a864661a60a8679cb6ead98f09aa0b41fa3
tirasYusop <yusopnoratira@gmail.com>
2026-04-16 21:13:09 +0800

    Init_admin fix

--- init_admin.py
+++ init_admin.py
@@ -1,28 +1,28 @@
+from django.contrib.auth import get_user_model
 import os
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_on_campus.settings")
 django.setup()
 
-from django.contrib.auth import get_user_model
-
 def create_admin():
     User = get_user_model()
 
-    username = "admin"
-    password = "admin123"
-    email = "admin@gmail.com"   # 👈 IMPORTANT FIX
+    if not User.objects.filter(username="admin").exists():
 
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
+        user = User.objects.create_superuser(
+            username="admin",
+            email="admin@gmail.com",
+            password="admin123"
         )
+
+        # force role AFTER creation (safer)
+        user.role = "admin"
+        user.save()
+
         print("Admin created")
     else:
         print("Admin already exists")
-
 
 if __name__ == "__main__":
     create_admin()
